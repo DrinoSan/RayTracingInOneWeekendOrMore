e8f468d0125292d21727a522edb94af5ca3b7258
Sandrino <s.becirbegovic@gmail.com>
2021-12-14 23:09:47 +0100

    Added Sphere Hitting function. - Simple Check if Ray hits Sphere

--- main.cpp
+++ main.cpp
@@ -5,10 +5,27 @@
 #include "Vec3.h"
 #include "Ray.h"
 
+bool hit_sphere(const point3& center, double radius, const Ray& r)
+{
+    Vec3 oc = r.origin() - center;
+
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+
+    return (discriminant > 0);
+}
+
 color ray_color(const Ray& r)
 {
+    if (hit_sphere(point3(0, 0, -1), 0.5, r))
+    {
+        return color(1, 0, 0);
+    }
     Vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5*(unit_direction.y() + 1.0);
+
     // If t == 1 we get end_value
     // If t == 0 we get start_value
     // The scaling of t is based on the y value of the unit vector
